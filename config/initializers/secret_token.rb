f0dc26d15c42a73b4f19e525f87424ace4c3023e
Larry Farrell <farrelldlfarrell@gmail.com>
2013-02-03 19:06:14 -0500

    Initial setup with hdyra, blacklight, sufia and typhoeus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ambry::Application.config.secret_token = '09e04c4a6fabc6712d1e3da1fffb1a2f751ca732653eb902767a8547d6cbca258013f6337807295270a5113729eece30daa652f90a0ce194199da692e309bfa2'
